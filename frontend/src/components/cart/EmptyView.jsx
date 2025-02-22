3818ec20726186bd1d05744c6491e4d61f370a19
Rucha Ambaliya <Rambaliyaforall@gmail.com>
2025-02-22 22:50:03 +0530

    Added dark theme for MedicineDetails.jsx and EmptyView.jsx (Cart)

--- frontend/src/components/cart/EmptyView.jsx
+++ frontend/src/components/cart/EmptyView.jsx
@@ -14,15 +14,15 @@ const EmptyView = (props) => {
             className="rounded-[80px]"
           />
         </div>
-        <h2>{msg}</h2>
-        <div className="text-center">
+        <h2 className="dark:text-white-1">{msg}</h2>
+        <div className="text-center dark:text-white-1">
           <p>Looks like you have not added anything in your cart.</p>
           <p>Go ahead and explore latest medicines.</p>
         </div>
         {link && (
           <Link
             to={link}
-            className="inline-block bg-blue-9 px-2 py-3 text-blue-1 rounded-[3px] transition-colors duration-200 ease-out hover:bg-blue-7"
+            className="inline-block bg-blue-9 px-2 py-3 text-blue-1 rounded-[3px] transition-colors duration-200 ease-out hover:bg-blue-7 dark:bg-blue-24 dark:hover:bg-blue-31 dark:active:bg-blue-31"
           >
             {btnText}
           </Link>
