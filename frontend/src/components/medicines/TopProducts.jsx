94423ed061a6b6d9a20991630f0d1579a8ad42eb
Rucha Ambaliya <Rambaliyaforall@gmail.com>
2025-02-24 15:17:57 +0530

    Added dark theme for Medicines.jsx, FeaturedProducts.jsx, TopProducts.jsx, and ProductCard.jsx

--- frontend/src/components/medicines/TopProducts.jsx
+++ frontend/src/components/medicines/TopProducts.jsx
@@ -21,7 +21,7 @@ const TopProducts = () => {
                     ))
                 }
                 {/* products_card browse_card */}
-                <div className="card w-[24%] max-xs:w-[98%] max-md:w-[46%] max-lg:w-[31%] border-[1px] border-white-1/40 max-h-[520px] shadow-[0_0_5px_2px_#ccc] rounded-[8px] text-blue-6 text-center transition-all duration-300 ease-in-out hover:shadow-[0_0_10px_2px_#7584AE] hover:text-blue-8 text-[1.5rem] flex items-center justify-start px-10 overflow-auto scrollbar-none mb-3">
+                <div className="card w-[24%] max-xs:w-[98%] max-md:w-[46%] max-lg:w-[31%] border-[1px] border-white-1/40 max-h-[520px] shadow-[0_0_5px_2px_#ccc] rounded-[8px] text-blue-6 text-center transition-all duration-300 ease-in-out hover:shadow-[0_0_10px_2px_#7584AE] hover:text-blue-8 text-[1.5rem] flex items-center justify-start px-10 overflow-auto scrollbar-none mb-3 dark:text-white-8">
                     <Link to="/all-medicines">
                         Browse All <br /> Medicines <BsArrowRight />
                     </Link>
